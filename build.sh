363211dcd4a314cad213a11ef6f347251012c7d7
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-09-21 11:47:30 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -34,26 +34,6 @@ cp -r "$PARENT_DIR/libKitsuneCommon/include" "$RESULT_DIR/"
 
 #-----------------------------------------------------------------------------------------------------------------
 
-# clone libKitsuneCommon
-git clone  git@gitlab.com:tobiasanker/libKitsuneCommon.git "$PARENT_DIR/libKitsuneCommon"
-cd "$PARENT_DIR/libKitsuneCommon"
-git checkout 0.5.3
-
-# create build directory for libKitsuneCommon and go into this directory
-LIB_KITSUNE_COMMON_DIR="$BUILD_DIR/libKitsuneCommon"
-mkdir -p $LIB_KITSUNE_COMMON_DIR
-cd $LIB_KITSUNE_COMMON_DIR
-
-# build libKitsuneCommon library with qmake
-/usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/libKitsuneCommon/libKitsuneCommon.pro" -spec linux-g++ "CONFIG += optimize_full"
-/usr/bin/make -j4
-
-# copy build-result and include-files into the result-directory
-cp "$LIB_KITSUNE_COMMON_DIR/src/libKitsuneCommon.so.0.5.3" "$RESULT_DIR/"
-cp -r "$PARENT_DIR/libKitsuneCommon/include" "$RESULT_DIR/"
-
-#-----------------------------------------------------------------------------------------------------------------
-
 # clone libKitsuneJson
 git clone  git@gitlab.com:tobiasanker/libKitsuneJson.git "$PARENT_DIR/libKitsuneJson"
 cd "$PARENT_DIR/libKitsuneJson"
